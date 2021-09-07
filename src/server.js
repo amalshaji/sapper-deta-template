391d4a6bea304dff0b4d7008404b3a2b6458abbd
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-09-07 22:58:50 +0530

    ⚡️ use express.static instead of sirv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-import sirv from 'sirv';
 import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
@@ -6,22 +5,16 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const app = express()
+app.use(
+	compression({threshold: 0}),
+	express.static('static'),
+	sapper.middleware()
+)
+
 if (dev) {
-	express() // You can also use Express
-		.use(
-			compression({ threshold: 0 }),
-			sirv('static', { dev }),
-			sapper.middleware()
-		).listen(3000)
+	app.listen(PORT || 3000)
 } else {
-	const app = express() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
 	module.exports = app
 }
 
-	
-// module.exports = app
